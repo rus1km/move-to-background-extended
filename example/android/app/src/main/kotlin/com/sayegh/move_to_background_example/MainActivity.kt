ce44eb659f1480efe05055892e10fc8c60344d60
Turin <r.mamedov@ringostat.com>
2025-07-17 10:27:02 +0300

    Fix gradle error, remove example

--- example/android/app/src/main/kotlin/com/sayegh/move_to_background_example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.sayegh.move_to_background_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ example/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,21 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<move_to_background/MoveToBackgroundPlugin.h>)
+#import <move_to_background/MoveToBackgroundPlugin.h>
+#else
+@import move_to_background;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [MoveToBackgroundPlugin registerWithRegistrar:[registry registrarForPlugin:@"MoveToBackgroundPlugin"]];
+}
+
+@end
